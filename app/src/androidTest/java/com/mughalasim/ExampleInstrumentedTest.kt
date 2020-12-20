847e719540373679809ee1a43e379ac639b0c371
Asim Mughal <asimkenya@gmail.com>
2020-12-20 14:33:52 +0000

    Will manage to write tests now that basic UI and functionality has been implemented

--- app/src/androidTest/java/com/mughalasim/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mughalasim/ExampleInstrumentedTest.kt
@@ -1,19 +1,19 @@
 package com.mughalasim
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.runners.JUnit4
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(JUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
@@ -21,4 +21,9 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.mughalasim", appContext.packageName)
     }
+
+    // Due to limited time constraints
+    // I was unable to write any tests although they would be written here
+
+
 }
